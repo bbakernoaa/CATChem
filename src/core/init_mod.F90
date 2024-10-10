8e984c0a237271194640ff06b728473b2b49e2c2
bbakernoaa <barry.baker@noaa.gov>
2024-10-10 13:52:56 -0400

    add chem and emis init to init_state and remove gridstate

--- src/core/init_mod.F90
+++ src/core/init_mod.F90
@@ -27,15 +27,13 @@ contains
    !!
    !! \ingroup core_modules
    !!!>
-   subroutine Init_Met(GridState, MetState, RC)
-      !
-      use GridState_Mod, Only : GridStateType
+   subroutine Init_Met(MetState, RC)
+      !Uses
       use MetState_Mod
       USE Error_Mod
       implicit none
 
       ! Arguments
-      TYPE(GridStateType), INTENT(IN)  :: GridState
       TYPE(MetStateType), INTENT(INOUT) :: MetState
       INTEGER,        INTENT(OUT) :: RC
 
@@ -47,7 +45,7 @@ contains
       ErrMsg = ''
       thisLoc = ' -> at Init_Met (in core/state_mod.F90)'
 
-      call Met_Allocate(GridState, MetState, RC)
+      call Met_Allocate(MetState, RC)
       if (RC /= CC_SUCCESS) then
          errMsg = 'Error allocating met state'
          call CC_Error(errMsg, RC , thisLoc)
@@ -66,19 +64,21 @@ contains
    !!
    !! \ingroup core_modules
    !!!>
-   subroutine Init_Diag(Config, DiagState, RC)
+   subroutine Init_Diag(Config, MetState, DiagState, ChemState, RC)
       use DiagState_Mod
       use Config_Opt_Mod, Only : ConfigType
-      use GridState_Mod, Only : GridStateType
+      use MetState_Mod, Only : MetStateType
+      use ChemState_Mod, Only : ChemStateType
       use Error_Mod
 
       implicit none
 
       ! Arguments
       TYPE(ConfigType),    INTENT(IN)    :: Config
-      ! TYPE(GridStateType), INTENT(IN)    :: GridState
+      TYPE(MetStateType),  INTENT(IN)    :: MetState
       TYPE(DiagStateType), INTENT(INOUT) :: DiagState
-      INTEGER,         INTENT(OUT) :: RC
+      TYPE(ChemStateType), INTENT(IN)    :: ChemState
+      INTEGER,             INTENT(OUT)   :: RC
 
       ! Local variables
       CHARACTER(LEN=255) :: ErrMsg, thisLoc
@@ -88,12 +88,96 @@ contains
       ErrMsg = ''
       thisLoc = ' -> at Init_Diag (in core/init_mod.F90)'
 
-      call Diag_Allocate(Config, DiagState, RC)
+      call Diag_Allocate(Config, MetState, ChemState, DiagState, RC)
       if (RC /= CC_SUCCESS) then
          errMsg = 'Error allocating diag state'
          call CC_Error(errMsg, RC , thisLoc)
       endif
 
    end subroutine Init_Diag
+
+   !> \brief Initialize the Chem state
+   !!
+   !! This subroutine allocates the Chem state.
+   !!
+   !! \param Config_Opt The config.
+   !! \param MetState The grid state containing information about the grid.
+   !! \param ChemState The Chem state to be initialized.
+   !! \param RC The return code.
+   !!
+   !! \ingroup core_modules
+   !!!>
+   subroutine Init_Chem(Config, MetState, ChemState, RC)
+      use ChemState_Mod
+      use Config_Opt_Mod, Only : ConfigType
+      use MetState_Mod, Only : MetStateType
+      use Error_Mod
+
+      implicit none
+
+      ! Arguments
+      TYPE(ConfigType),    INTENT(IN)    :: Config
+      TYPE(MetStateType),  INTENT(IN)    :: MetState
+      TYPE(ChemStateType), INTENT(INOUT) :: ChemState
+
+      INTEGER, INTENT(OUT) :: RC
+
+      ! Local variables
+      CHARACTER(LEN=255) :: ErrMsg, thisLoc
+
+      ! Initialize
+      RC = CC_SUCCESS
+      ErrMsg = ''
+      thisLoc = ' -> at Init_Diag (in core/init_mod.F90)'
+
+      ! call Diag_Allocate(Config, MetState, DiagState, RC)
+      ! if (RC /= CC_SUCCESS) then
+      !    errMsg = 'Error allocating diag state'
+      !    call CC_Error(errMsg, RC , thisLoc)
+      ! endif
+
+   end subroutine Init_Chem
+
+   !> \brief Initialize the Chem state
+   !!
+   !! This subroutine allocates the Chem state.
+   !!
+   !! \param Config_Opt The config.
+   !! \param MetState The grid state containing information about the grid.
+   !! \param ChemState The Chem state to be initialized.
+   !! \param RC The return code.
+   !!
+   !! \ingroup core_modules
+   !!!>
+   subroutine Init_Emis(Config, MetState, EmisState, RC)
+      use EmisState_Mod
+      use Config_Opt_Mod, Only : ConfigType
+      use MetState_Mod, Only : MetStateType
+      use Error_Mod
+
+      implicit none
+
+      ! Arguments
+      TYPE(ConfigType),    INTENT(IN)    :: Config
+      TYPE(MetStateType),  INTENT(IN)    :: MetState
+      TYPE(EmisStateType), INTENT(INOUT) :: EmisState
+
+      INTEGER, INTENT(OUT) :: RC
+
+      ! Local variables
+      CHARACTER(LEN=255) :: ErrMsg, thisLoc
+
+      ! Initialize
+      RC = CC_SUCCESS
+      ErrMsg = ''
+      thisLoc = ' -> at Init_Diag (in core/init_mod.F90)'
+
+      ! call Diag_Allocate(Config, MetState, DiagState, RC)
+      ! if (RC /= CC_SUCCESS) then
+      !    errMsg = 'Error allocating diag state'
+      !    call CC_Error(errMsg, RC , thisLoc)
+      ! endif
+
+   end subroutine Init_Emis
 
 end module init_mod
