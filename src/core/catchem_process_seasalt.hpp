3adddb0c8072c4b18e12faffa29cb6a9de50c393
bbakernoaa <barry.baker@noaa.gov>
2026-07-08 15:21:20 -0400

    feat(core): implement C++ process wrappers for physical schemes

---
+++ src/core/catchem_process_seasalt.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "catchem_process_interface.hpp"
+#include <functional>
+
+namespace catchem {
+
+class SeaSaltProcess : public ProcessInterface {
+private:
+    std::string active_scheme;
+    std::function<void(void*)> fortran_callback;
+
+public:
+    SeaSaltProcess();
+    std::string get_name() const override { return "seasalt"; }
+    void init(std::shared_ptr<StateManager> state) override;
+    void set_fortran_bridge_callback(std::function<void(void*)> cb);
+    void run(std::shared_ptr<StateManager> state) override;
+    void finalize() override {}
+};
+
+} // namespace catchem
